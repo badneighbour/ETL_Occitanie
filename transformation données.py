4ea911bcc66d3d68673b49b8be2ae1eaf16aa883
David <davidmalvoisin@hotmail.fr>
2024-07-28 00:32:36 +0200

    regroupement de la valeur locative et de la valeur foncière et calcul du rendement locatif

--- transformation données.py
+++ transformation données.py
@@ -35,7 +35,12 @@ def dowload_data(url, file):
     nom_dossier, nom_fichier = file.split("/")
     if pathlib.Path(file).is_file():
         if not TELECHARGER:
-            print("Le fichier '" + nom_fichier + "' existe déjà dans le dossier '" + nom_dossier + "'. Téléchargement annulé.")
+            print("Le fichier '"
+                  + nom_fichier
+                  + "' existe déjà dans le dossier '"
+                  + nom_dossier
+                  + "'. Téléchargement annulé."
+                  )
             return
         pathlib.Path(file).unlink()
     urllib.request.urlretrieve(url, file)
@@ -80,7 +85,6 @@ DELINQUANCE = DELINQUANCE.filter(pl.col("CODGEO_2023").str.contains(regex_codes_
 VF = VF[
     [
         "Valeur fonciere",
-        "Code postal",
         "Code departement",
         "Code commune",
         "Code type local",
@@ -90,10 +94,10 @@ VF = VF[
     ]
 ]
 
-VLAPP.drop("TYPPRED", "nbobs_com", "nbobs_mail")
-VLM.drop("TYPPRED", "nbobs_com", "nbobs_mail")
-VLAPP12.drop("TYPPRED", "nbobs_com", "nbobs_mail")
-VLAPP3PLUS.drop("TYPPRED", "nbobs_com", "nbobs_mail")
+VLAPP = VLAPP.drop("TYPPRED", "nbobs_com", "nbobs_mail", "id_zone")
+VLM = VLM.drop("TYPPRED", "nbobs_com", "nbobs_mail", "id_zone")
+VLAPP12 = VLAPP12.drop("TYPPRED", "nbobs_com", "nbobs_mail", "id_zone")
+VLAPP3PLUS = VLAPP3PLUS.drop("TYPPRED", "nbobs_com", "nbobs_mail", "id_zone")
 
 # On renomme la colonne pour plus de clareté
 CC = CC.rename({"#Code_commune_INSEE": "INSEE_C"})
@@ -101,7 +105,7 @@ CC = CC.rename({"#Code_commune_INSEE": "INSEE_C"})
 # On enlève tous les locaux industriels et les dépendances
 VF = VF.filter(pl.col("Code type local") < 3)
 
-# On joint rajoute la donnée code insee manquante qui nous sert pour les jointures
+# On remplace le code departement et le code commune par le code insee qui nous sert pour les jointures
 VF = VF.with_columns(
     pl.when(pl.col("Code commune") < 10)
     .then(pl.col("Code departement").cast(pl.String) + pl.lit("00") + pl.col("Code commune").cast(pl.String))
@@ -111,20 +115,60 @@ VF = VF.with_columns(
     .alias("INSEE_C")
 )
 
-# On rajoute une colonne pour le type de bien de location
+VF = VF.drop("Code commune", "Code departement")
+
+# On rajoute une colonne pour le type de bien
+VLAPP = VLAPP.with_columns(pl.lit("appartement").alias("Type_de_bien"))
+VLAPP12 = VLAPP12.with_columns(pl.lit("appartement de 1 ou 2 pièces").alias("Type_de_bien"))
+VLAPP3PLUS = VLAPP3PLUS.with_columns(pl.lit("appartement de 3 pièces et plus").alias("Type_de_bien"))
+VLM = VLM.with_columns(pl.lit("maison").alias("Type_de_bien"))
+VF = VF.with_columns(
+    pl.when(pl.col("Code type local").eq(1))
+    .then(pl.lit("maison"))
+    .when(pl.col("Code type local").eq(2) & pl.col("Nombre pieces principales") < 3)
+    .then(pl.lit("appartement de 1 ou 2 pièces"))
+    .when(pl.col("Code type local").eq(2) & pl.col("Nombre pieces principales") > 2)
+    .then(pl.lit("appartement de 3 pièces et plus"))
+    .otherwise(pl.lit("autre"))
+    .alias("Type_de_bien")
+).drop("Code type local", "Type local")
+
+# On fait l'union de toutes les données sur la valeur locative dans un même Dataframe
+VL = pl.concat([VLM, VLAPP, VLAPP12, VLAPP3PLUS])
+
+# On ajoute un prix au metre carré
+VF = VF.with_columns((pl.col("Valeur fonciere") / pl.col("Surface reelle bati")).alias("valeur_fonciere_m2"))
+
+# On fait les moyennes selon le type de bien
+VFMOY = VF.group_by(
+    ["INSEE_C", "Type_de_bien"]
+).agg(pl.col("valeur_fonciere_m2").mean())
+
+VFMOYAPP = VF.filter(pl.col("Type_de_bien").str.contains("app")).group_by(
+    ["INSEE_C"]
+).agg(pl.lit("appartement").alias("Type_de_bien"), pl.col("valeur_fonciere_m2").mean())
+
+VFMOY = pl.concat([VFMOY, VFMOYAPP])
+
+# On concatène la VL et la VF et on concalcule le rendement locatif
+RL = (VL.join(VFMOY, on=["INSEE_C", "Type_de_bien"], how="left")
+      .with_columns(pl.col("valeur_fonciere_m2") / pl.col("loypredm2") * pl.lit(12 * 100)))
 
 with pl.Config(tbl_cols=-1) and pl.Config(tbl_width_chars=1000) and pl.Config(tbl_rows=40):
     print(VLM.sort("INSEE_C"))
     print(VLAPP12.sort("INSEE_C"))
     print(VLAPP3PLUS.sort("INSEE_C"))
     print(VLAPP.sort("INSEE_C"))
+    print(VL.sort("INSEE_C"))
     print(VF.sort("INSEE_C"))
+    print(VFMOY.sort("INSEE_C"))
+    print(VFMOYAPP.sort("INSEE_C"))
+    print(RL.sort("INSEE_C"))
     print(CC.sort("INSEE_C"))
-    print(CC.unique("Code_postal"))
     print(DELINQUANCE.sort("CODGEO_2023"))
 
 # On sauvegarde les données traitées
-SAUVEGARDER = True
+SAUVEGARDER = False
 output_path = "output/"
 output1 = output_path + "valeursfoncieres-2023.csv"
 output2 = output_path + "carte_loyers_maison.csv"
